d4d2037cf20350fd548e3706ca83eeb3ed9eca8b
yujinK <yujinkim.dev@gmail.com>
2020-12-01 23:54:32 +0900

    feat: toolbar menu

--- Android/app/src/main/java/com/yujin/onionmarket/MainActivity.kt
+++ Android/app/src/main/java/com/yujin/onionmarket/MainActivity.kt
@@ -2,6 +2,8 @@ package com.yujin.onionmarket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
