17b13f1ba76d4ba6bb56c35a877caf988aea3377
yujinK <yujinkim.dev@gmail.com>
2020-12-02 01:11:56 +0900

    feat: SwipeRefreshLayout

--- Android/app/src/main/java/com/yujin/onionmarket/HomeFragment.kt
+++ Android/app/src/main/java/com/yujin/onionmarket/HomeFragment.kt
@@ -1,6 +1,24 @@
 package com.yujin.onionmarket
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        swipeRefreshLayout = view.findViewById(R.id.swipe_layout)
+        swipeRefreshLayout.setOnRefreshListener {
+            refresh()
+        }
+    }
+
+    private fun refresh() {
+        Toast.makeText(requireContext(), "refresh!!!", Toast.LENGTH_SHORT).show()
+        swipeRefreshLayout.isRefreshing = false
+    }
 }
