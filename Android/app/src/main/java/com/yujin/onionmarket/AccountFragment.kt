1ddfa364093571e27c711d522bd1b9b658eb3c5f
yujinK <yujinkim.dev@gmail.com>
2020-12-21 14:16:05 +0900

    feat: profile layout 수정

--- Android/app/src/main/java/com/yujin/onionmarket/AccountFragment.kt
+++ Android/app/src/main/java/com/yujin/onionmarket/AccountFragment.kt
@@ -1,6 +1,20 @@
 package com.yujin.onionmarket
 
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
 
 class AccountFragment : Fragment(R.layout.fragment_account) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init(view);
+    }
+
+    private fun init(view: View) {
+        val viewProfile = view.findViewById<AppCompatButton>(R.id.btn_view_profile)
+        viewProfile.setOnClickListener {
+
+        }
+    }
 }
