5b33166f9c06566b08f108152b5145569ba0ecb5
yujinK <yujinkim.dev@gmail.com>
2020-12-22 18:48:20 +0900

    feat: layout 설정
    - login
    - sign up

--- Android/app/src/main/java/com/yujin/onionmarket/LoginActivity.kt
+++ Android/app/src/main/java/com/yujin/onionmarket/LoginActivity.kt
@@ -1,11 +1,27 @@
 package com.yujin.onionmarket
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        init()
+    }
+
+    private fun init() {
+        val join = findViewById<TextView>(R.id.tv_sign_up)
+        join.setOnClickListener {
+            join()
+        }
+    }
+
+    private fun join() {
+        val intent = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
