96f61d07200f4870478e03c589137f7ed2752370
yujinK <yujinkim.dev@gmail.com>
2021-01-16 00:09:08 +0900

    feat: add margin to toolbar
    - status bar와 겹치지 않도록

--- Android/app/src/main/java/com/yujin/onionmarket/view/DetailSaleActivity.kt
+++ Android/app/src/main/java/com/yujin/onionmarket/view/DetailSaleActivity.kt
@@ -1,11 +1,14 @@
 package com.yujin.onionmarket.view
 
+import android.app.Activity
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.ViewGroup
 import android.view.WindowManager
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import com.yujin.onionmarket.R
 
@@ -17,11 +20,25 @@ class DetailSaleActivity : AppCompatActivity() {
     }
 
     private fun init() {
+        makeStatusBarTransparent()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.ll_detail)) { _, insets ->
+            findViewById<Toolbar>(R.id.toolbar).setMarginTop(insets.systemWindowInsetTop)
+            insets.consumeSystemWindowInsets()
+        }
+    }
+
+    private fun Activity.makeStatusBarTransparent() {
         this.window.apply {
             clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
             addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
             decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
             statusBarColor = Color.TRANSPARENT
         }
+    }
+
+    fun View.setMarginTop(marginTop: Int) {
+        val menuLayoutParams = this.layoutParams as ViewGroup.MarginLayoutParams
+        menuLayoutParams.setMargins(0, marginTop, 0, 0)
+        this.layoutParams = menuLayoutParams
     }
 }
